194e03dd7e73bb307c45b721934ca0eac6846e2f
seal0207 <50684179+seal0207@users.noreply.github.com>
2020-02-16 17:18:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -299,7 +299,7 @@ modify_nginx_other(){
 web_camouflage(){
     ##请注意 这里和LNMP脚本的默认路径冲突，千万不要在安装了LNMP的环境下使用本脚本，否则后果自负
     rm -rf /home/wwwroot && mkdir -p /home/wwwroot && cd /home/wwwroot
-    git clone https://github.com/wulabing/3DCEList.git
+    git clone https://github.com/seal0207/yqjk.git
     judge "web 站点伪装"
 }
 v2ray_install(){
@@ -538,7 +538,7 @@ nginx_conf_add(){
         ssl_ciphers           TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
         server_name           serveraddr.com;
         index index.html index.htm;
-        root  /home/wwwroot/3DCEList;
+        root  /home/wwwroot/yqjk;
         error_page 400 = /400.html;
         location /ray/
         {
@@ -628,7 +628,7 @@ vmess_qr_config_tls_ws(){
     cat > $v2ray_qr_config_file <<-EOF
 {
   "v": "2",
-  "ps": "wulabing_${domain}",
+  "ps": "Seak-${domain}",
   "add": "${domain}",
   "port": "${port}",
   "id": "${UUID}",
@@ -646,7 +646,7 @@ vmess_qr_config_h2(){
     cat > $v2ray_qr_config_file <<-EOF
 {
   "v": "2",
-  "ps": "wulabing_${domain}",
+  "ps": "Seal-${domain}",
   "add": "${domain}",
   "port": "${port}",
   "id": "${UUID}",
@@ -717,7 +717,6 @@ nginx_systemd(){
 [Unit]
 Description=The NGINX HTTP and reverse proxy server
 After=syslog.target network.target remote-fs.target nss-lookup.target
-
 [Service]
 Type=forking
 PIDFile=/etc/nginx/logs/nginx.pid
@@ -726,7 +725,6 @@ ExecStart=/etc/nginx/sbin/nginx -c ${nginx_dir}/conf/nginx.conf
 ExecReload=/etc/nginx/sbin/nginx -s reload
 ExecStop=/bin/kill -s QUIT \$MAINPID
 PrivateTmp=true
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -1019,4 +1017,3 @@ menu(){
 
 judge_mode
 list $1
-
